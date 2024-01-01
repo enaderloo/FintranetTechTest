dac099b815e1b859ae700d8d616853057077581c
Ebrahim Naderloo <e.naderloo@gmail.com>
2024-01-01 16:39:56 +0330

    Update HolidayChecker.cs

--- FintranetTechTest.Application/Services/HolidayChecker.cs
+++ FintranetTechTest.Application/Services/HolidayChecker.cs
@@ -7,15 +7,15 @@ namespace FintranetTechTest.Application.Services
     {
         private readonly List<PublicHoliday> publicHolidays = new()
         {
-        new PublicHoliday(1, 1),
-        new PublicHoliday(3, 28, 29),
-        new PublicHoliday(4, 1, 30),
-        new PublicHoliday(5, 1, 8, 9),
-        new PublicHoliday(6, 5, 6, 21),
-        new PublicHoliday(7),
-        new PublicHoliday(11, 1),
-        new PublicHoliday(12, 24, 25, 26, 31)
-    };
+            new PublicHoliday(1, 1),
+            new PublicHoliday(3, 28, 29),
+            new PublicHoliday(4, 1, 30),
+            new PublicHoliday(5, 1, 8, 9),
+            new PublicHoliday(6, 5, 6, 21),
+            new PublicHoliday(7),
+            new PublicHoliday(11, 1),
+            new PublicHoliday(12, 24, 25, 26, 31)
+        };
 
         public bool IsPublicHoliday(int year, int month, int day)
         {
